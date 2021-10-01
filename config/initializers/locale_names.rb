01bc7e49b7c3ff87ac9850233ccc091b45018969
daithoms <91745476+daithoms@users.noreply.github.com>
2021-10-01 16:41:04 +0100

    Add files via upload

---
+++ config/initializers/locale_names.rb
@@ -0,0 +1,37 @@
+# Be sure to restart your server when you modify this file.
+
+LOCALE_NAMES = {
+  am: 'Amharic',
+  ar: 'Arabic',
+  bn: 'Bengali',
+  en: 'English',
+  es: 'Spanish',
+  fi: 'Finnish',
+  fr: 'French',
+  hi: 'Hindi',
+  ht: 'Haitian Creole',
+  ja: 'Japanese',
+  km: 'Khmer',
+  pl: 'Polish',
+  pt: 'Portuguese',
+  tl: 'Tagalog/Filipino',
+  zh: 'Chinese',
+}
+
+LOCAL_LOCALE_NAMES = {
+  am: 'አማርኛ',
+  ar: 'اللغة العربية',
+  bn: 'বাংলা ভাষা',
+  en: 'English',
+  es: 'Español',
+  fi: 'Suomi',
+  fr: 'Français',
+  hi: 'हिन्दी',
+  ht: 'Kreyòl ayisyen',
+  ja: '日本語',
+  km: 'ភាសាខ្មែរ',
+  pl: 'Polski',
+  pt: 'Português',
+  tl: 'Tagalog/Filipino',
+  zh: '中文',
+}
